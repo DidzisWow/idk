26763d710ce1833349f3c7180a07175cf5d7d554
DidzisWow <didzisghill@gmail.com>
2026-01-26 15:20:12 +0200

    Boom

--- Database.php
+++ Database.php
@@ -4,17 +4,11 @@ class Database {
     private $pdo;
 
     public function __construct($config) {
-        $config = [
-            "host" => "localhost",
-            "port" => 3306,
-            "user" => "root",
-            "password" => "",
-            "dbname" => "idk",
-            "charset" => "utf8mb4"
-        ];
+
         $dsn = "mysql:" . http_build_query($config, arg_separator: ";");
-    $this->pdo = new PDO($dsn);
-    $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo = new PDO($dsn);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
 }
 
     public function query($sql) {
