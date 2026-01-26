25ac8ec693ddd4473cb3218f1c6ae8c34d68406f
DidzisWow <didzisghill@gmail.com>
2026-01-26 14:56:21 +0200

    Boom

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
 <?php
 
+require_once "functions.php";
+require_once "Database.php";
+$config = require "config.php";
 
-$dsn = "mysql:host=localhost;port=3306;dbname=idk;charset=utf8mb4";
-$user = "root";
-$password = "";
-
-$pdo = new PDO($dsn, $user, $password);
-
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
-
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config["database"]);
+$posts = $db->query("SELECT * FROM posts");
 
 echo "<ul>";
     foreach($posts as $post) {
-    echo "<li>" . $post["content"] . "</li>";
-}
+        echo "<li>" . $post["content"] . "</li>";
+    }
 echo "</ul>";
-
 
 
 
