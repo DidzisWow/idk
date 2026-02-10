3b68ff9c2d6f13847ec48ae39afefaed7af68467
DidzisWow <didzisghill@gmail.com>
2026-02-10 09:22:26 +0200

    (0_0)

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 
-
 require_once "functions.php";
 require_once "Database.php";
 $config = require "config.php";
 
 $db = new Database($config["database"]);
-$sql_query = "SELECT * FROM posts"; $params = []; 
+$sql_query = "SELECT * FROM posts"; $params = [];
+$params = [];
+
 if (isset($_GET["search_query"]) && trim($_GET["search_query"]) !== "") { 
 $sql_query .= " WHERE content LIKE :search"; 
-$params["search"] = "%" . trim($_GET["search_query"]) . "%"; }
+$params["search"] = "%" . trim($_GET["search_query"]) . "%"; 
+}
 
 $posts = $db->query($sql_query, $params)->fetchAll(PDO::FETCH_ASSOC);
 
+require "./skatos/index.view.php";
 
 echo "<h1>Blogs</h1>";
 
